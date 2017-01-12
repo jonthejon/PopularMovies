935df53c9fff92df6bb69d9cb9d57a0165b2f5e4
Jonathan Oliveira <jonathan@jonathanoliveira.org>
2017-01-12 15:51:47 -0200

    Create GridAdapter class and update mainactivity layout
    GridAdapter is the adapter for the Recycler View
    Inside the mainactivity layout, we putted a RecyclerView as a single child of a FrameLayout

--- app/src/main/java/jonathanoliveira/org/popularmovies/MainActivity.java
+++ app/src/main/java/jonathanoliveira/org/popularmovies/MainActivity.java
@@ -2,16 +2,20 @@ package jonathanoliveira.org.popularmovies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView mRecyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRecyclerView = (RecyclerView) findViewById(R.id.rv_grid);
     }
 
     @Override
