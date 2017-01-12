a4f5c5ac0d07b9de6b79cee8a09dcc741ece42ed
Jonathan Oliveira <jonathan@jonathanoliveira.org>
2017-01-12 14:46:11 -0200

    Create detailsActivity that will show the movies details

--- app/src/main/java/jonathanoliveira/org/popularmovies/MainActivity.java
+++ app/src/main/java/jonathanoliveira/org/popularmovies/MainActivity.java
@@ -37,7 +37,9 @@ public class MainActivity extends AppCompatActivity {
 // completed: 12/01/17 (1.2) create a new item called settings in the menu xml with proper properties such as order, if room, etc
 // completed: 12/01/17 (1.3) override the proper methods to create the menu and implement them
 // completed: 12/01/17 (1.4) on the itemclicked method, create a Toast to check if button is working
-// TODO: 12/01/17 (2) create new detailsActivity and link it as a child to the main activity
+// completed: 12/01/17 (2) create new detailsActivity and link it as a child to the main activity
+// completed: 12/01/17 (2.1) create a new activity named detailsActivity with the help of the wizard
+// completed: 12/01/17 (2.2) update the manifest.xml to proper tag the child activity to the main activity
 // TODO: 12/01/17 (3) create new Layout that will hold all images from the server API in Grid
 // TODO: 12/01/17 (4) create Adapter that will give the Grid all the views that it needs
 // TODO: 12/01/17 (5) create a grid view using the RecyclerView and chain it with the Adapter
