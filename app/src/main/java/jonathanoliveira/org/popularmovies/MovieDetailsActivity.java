d6b35256a4e6be23002c59df13fef282006bb5a8
Jonathan Oliveira <jonathan@jonathanoliveira.org>
2017-01-14 00:55:00 -0200

    DetailsActivity is identifying the callerMovie object and reading data. It displays the movie Title, Release Date and Overview.

--- app/src/main/java/jonathanoliveira/org/popularmovies/MovieDetailsActivity.java
+++ app/src/main/java/jonathanoliveira/org/popularmovies/MovieDetailsActivity.java
@@ -8,19 +8,27 @@ import android.widget.TextView;
 public class MovieDetailsActivity extends AppCompatActivity {
 
     private TextView movieNameTextView;
+    private TextView movieOverviewTextView;
+    private TextView movieRDTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_details);
         movieNameTextView = (TextView) findViewById(R.id.movie_name_detail);
+        movieOverviewTextView = (TextView) findViewById(R.id.movie_overview_detail);
+        movieRDTextView = (TextView) findViewById(R.id.movie_release_date_detail);
 
         Intent callerIntent = getIntent();
 
         if (callerIntent.hasExtra(Intent.EXTRA_TEXT)) {
             Movie callerMovie = (Movie) callerIntent.getSerializableExtra(Intent.EXTRA_TEXT);
             String movieName = callerMovie.getMovie_title();
+            String movieOverview = callerMovie.getOverview();
+            String movieReleaseDate = callerMovie.getRelease_date();
             movieNameTextView.setText(movieName);
+            movieOverviewTextView.setText(movieOverview);
+            movieRDTextView.setText(movieReleaseDate);
         }
     }
 }
